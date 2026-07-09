372ec767965050e16d407e6f98595ff2bc49beaf
Vijayavel_R <vijayavel.Rajasekaran@ctg.com>
2026-07-09 15:26:02 +0530

    mysql settings

---
+++ backend/app/database/connection.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from app.config import settings
+
+DATABASE_URL = (
+    f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}"
+    f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+)
+
+engine = create_engine(DATABASE_URL)
+
+print("Database Connected Successfully!")
